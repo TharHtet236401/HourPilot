dcaa8e836c5f80d3f7b7a7c8b079f2af1d7a31f9
TharHtet236401 <tharhteta3@gmail.com>
2026-08-16 08:44:03 +0100

    seperate the model files

--- shifts/models.py
+++ shifts/models.py
@@ -7,28 +7,7 @@ from decimal import Decimal
 from django.conf import settings
 from django.db import models
 
-
-class Workplace(models.Model):
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name="workplaces",
-    )
-
-    name = models.CharField(max_length=100)
-
-    hourly_rate = models.DecimalField(
-        max_digits=6,
-        decimal_places=2,
-    )
-
-    is_active = models.BooleanField(default=True)
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.name
+from workspaces.models import Workplace
 
 
 class Shift(models.Model):
---
+++ workspaces/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.conf import settings
+
+class Workplace(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="workplaces",
+    )
+
+    name = models.CharField(max_length=100)
+
+    hourly_rate = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+    )
+
+    is_active = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
