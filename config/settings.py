2556baa18c81391485e4f5d080eff721fa6a7894
TharHtet236401 <tharhteta3@gmail.com>
2026-09-06 13:25:06 +0100

    product static files

--- config/settings.py
+++ config/settings.py
@@ -140,6 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 LOGIN_REDIRECT_URL = "/"
